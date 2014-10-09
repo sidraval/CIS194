e3880f1cce635230ad4d59b6be5822c66d3de271
Sid Raval <sidsraval@gmail.com>
2014-10-08 23:19:36 -0400

    Horrible solution to histogram

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -14,10 +14,19 @@ localMaxima (x:[]) = []
 localMaxima (x:y:[]) = []
 localMaxima (x:y:z:others) = isLocalMaxima (x:y:z:[]) ++ localMaxima (y:z:others)
 
+histogram :: [Integer] -> String
+histogram = unlines . reverse . map oneLine . mappedData
+
+mappedData :: [Integer] -> [[Integer]]
+mappedData xs = filter (not . null) $ foldl (\accum el -> accum ++ [reduceData . last $ accum]) [xs] xs
+
+reduceData :: [Integer] -> [Integer]
+reduceData xs = foldl (\accum el -> delete el accum) xs [0..9]
+
 oneLine :: [Integer] -> String
 oneLine dataSet = map (starOrEmpty dataSet) [0..9]
 
-starOrEmpty :: [[Integer]] -> Integer -> Char
+starOrEmpty :: [Integer] -> Integer -> Char
 starOrEmpty y x
   | x `elem` y = '*'
   | otherwise = ' '

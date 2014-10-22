f523b7892fb7f6f4b7e36089154de7a2cba1aeb0
Anna Kopp and Sid Raval <pair+akopp+sraval@pivotallabs.com>
2014-10-21 22:54:34 -0400

    Skips solutions, some QuickCheck invariants

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -3,6 +3,15 @@
 module Golf where
 
 import Data.List
+import Test.HUnit
+import Test.QuickCheck
+
+skips :: [a] -> [[a]]
+skips xs = map (\n -> everyNth xs n) [1..(length xs)]
+
+everyNth xs n
+  | (length xs) < n = []
+  | otherwise = take 1 (drop (n-1) xs) ++ (everyNth (drop n xs) n)
 
 isLocalMaxima :: [Integer] -> [Integer]
 isLocalMaxima (x:y:z:_)
@@ -17,10 +26,10 @@ histogram :: [Integer] -> String
 histogram = unlines . map oneLine . mappedData
 
 mappedData :: [Integer] -> [[Integer]]
-mappedData xs = filter (not . null) $ foldl (\accum el -> [reduceData . head $ accum] ++ accum) [xs] xs
+mappedData xs = filter (not . null) $ foldl (\accum el -> [reduceData $ take 1 accum] ++ accum) [xs] xs
 
-reduceData :: [Integer] -> [Integer]
-reduceData xs = foldl (\accum el -> delete el accum) xs [0..9]
+reduceData :: [[Integer]] -> [Integer]
+reduceData (x:xs) = foldl (\accum el -> delete el accum) x [0..9]
 
 oneLine :: [Integer] -> String
 oneLine dataSet = map (starOrEmpty dataSet) [0..9]
@@ -29,3 +38,14 @@ starOrEmpty :: [Integer] -> Integer -> Char
 starOrEmpty y x
   | x `elem` y = '*'
   | otherwise = ' '
+
+-- QuickCheck Invariants
+
+invarianceOfLength s = length (skips s) == length s
+firstElement s
+  | length s > 0 = head (skips s) == s
+  | otherwise = skips s == []
+ 
+lastElement s
+  | length s > 0 = head (last (skips s)) == last s
+  | otherwise = skips s == []

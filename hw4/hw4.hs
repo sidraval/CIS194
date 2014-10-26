26c42fbd6bda65538f8bed37586959f5b8f028c3
Sid Raval <sidsraval@gmail.com>
2014-10-25 21:11:00 -0400

    Quick and dirty sieve solution

--- hw4/hw4.hs
+++ hw4/hw4.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 fun1 :: [Integer] -> Integer
 fun1 = foldl (\accum x -> (x-2) * accum)  1 . filter even
 fun1' = product . map (subtract 2) . filter even
@@ -19,3 +21,18 @@ xor = foldl (\accum el -> (accum || el) && (not (accum && el))) False
 
 mapz :: (a -> b) -> [a] -> [b]
 mapz f = foldl (\accum el -> accum ++ [f el]) []
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map (\x -> 2 * x + 1) ([1..n] \\ (constrained n))
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys]
+
+constrained n = map removalForm (filteredNums n)
+
+removalForm (x, y) = x + y + 2 * x * y
+
+filteredNums n = filter (constraints n) $ cartProd [1..n] [1..n]
+
+constraints :: Integer -> (Integer, Integer) -> Bool
+constraints n (x, y) = (x <= y) && (x + y + (2 * x * y) <= n)

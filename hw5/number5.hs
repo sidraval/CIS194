5e3da99270612174bae10fa0a55693746bfe1f74
Sid Raval <sidsraval@gmail.com>
2014-11-04 20:29:07 -0500

    More hw5

--- hw5/number5.hs
+++ hw5/number5.hs
@@ -5,32 +5,10 @@ module Calc where
 import Parser
 import StackVM
 
-data ExprT = Lit Integer
-           | Add ExprT ExprT
-           | Mul ExprT ExprT
-  deriving (Show, Eq)
-
-eval :: ExprT -> Integer
-eval (Lit i) = i
-eval (Add x y) = eval x + eval y
-eval (Mul x y) = eval x * eval y
-
-evalStr :: String -> Maybe Integer
-evalStr = maybeEval . parseExp Lit Add Mul
-
-maybeEval :: Maybe ExprT -> Maybe Integer
-maybeEval (Just t) = Just (eval t)
-maybeEval Nothing = Nothing
-
 class Expr a where
   lit :: Integer -> a
   mul :: a -> a -> a
   add :: a -> a -> a
-
-instance Expr ExprT where
-  lit x = Lit x
-  mul = Mul
-  add = Add
 
 instance Expr Integer where
   lit k = k
@@ -51,6 +29,3 @@ instance Expr MinMax where
   lit k = MinMax k
   mul (MinMax x) (MinMax y) = MinMax (min x y)
   add (MinMax x) (MinMax y) = MinMax (max x y)
-
-{- instance Expr Program where -}
-  {- lit V -}

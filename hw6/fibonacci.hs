dfa56075c640ac0658940b86215e382e1eb76f04
Sid Raval <sidsraval@gmail.com>
2014-11-11 22:03:10 -0500

    More of HW6

--- hw6/fibonacci.hs
+++ hw6/fibonacci.hs
@@ -17,3 +17,28 @@ fibs2' [0,1] = [0,1]
 fibs2' xs = (xs ++ [fib' $ reverse xs])
 
 fib' = sum . take 2
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x y) = x : streamToList y
+
+streamFromList :: [a] -> Stream a
+streamFromList (x:xs) = Cons x (streamFromList xs)
+
+instance Show a => Show (Stream a) where
+  show xs = show $ take 20 $ streamToList xs
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons (f x) (streamFromSeed f (f x))
+
+nats :: Stream Integer
+nats = streamFromList [0..]
+
+ruler :: Stream Integer
